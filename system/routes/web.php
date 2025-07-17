d778f985f377fd1aa542bc14c27a0e04121dca99
davigabrielsp <davigabriel.sp@outlook.com>
2025-07-16 21:11:32 -0300

    view and controller contas

--- system/routes/web.php
+++ system/routes/web.php
@@ -1,18 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-Route::get('/', function () {
+use App\http\Controllers\ContaController;
+Route::get('/', function(){
     return view('welcome');
 });
+
+Route::get('/conta', [ContaController::class, 'index'])->name('conta.index');
+Route::get('/conta/create', [ContaController::class, 'create'])->name('conta.create');
+Route::post('/conta/store', [ContaController::class, 'store'])->name('conta.store');
+Route::get('/conta/show', [ContaController::class, 'show'])->name('conta.show');
+Route::get('/conta/edit', [ContaController::class, 'edit'])->name('conta.edit');
+Route::put('/conta/update', [ContaController::class, 'update'])->name('conta.update');
+Route::delete('/conta/destroy', [ContaController::class, 'destroy'])->name('conta.destroy');
+
+
